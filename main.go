8ac9640b21b44250122c127417f53a245187ebae
Fauzi Sofyan <fauzilax@gmail.com>
2023-02-23 19:24:12 +0700

    change folder name

--- main.go
+++ main.go
@@ -2,19 +2,19 @@ package main
 
 import (
 	"fmt"
-	"hackerrank/golang"
+	"hackerrank/problem"
 )
 
 func main() {
 	// Solve Me First
-	fmt.Println(golang.SolveMeFirst(30, 40))
+	fmt.Println(problem.SolveMeFirst(30, 40))
 
 	// Simple Array Sum
 	simpleArraySum := []int32{2, 3, 4, 2, 5, 12, 20}
-	fmt.Println(golang.SimpleArraySum(simpleArraySum))
+	fmt.Println(problem.SimpleArraySum(simpleArraySum))
 
 	// Compare The Triplets
 	a := []int32{5, 6, 7}
 	b := []int32{3, 6, 10}
-	fmt.Println(golang.CompareTriplets(a, b))
+	fmt.Println(problem.CompareTriplets(a, b))
 }
--- problem/solvemefirst.go
+++ problem/solvemefirst.go
@@ -1,4 +1,4 @@
-package golang
+package problem
 
 func SolveMeFirst(a uint32, b uint32) uint32 {
 	return a + b
